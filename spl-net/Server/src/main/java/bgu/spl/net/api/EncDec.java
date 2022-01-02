01d0682f13463d29af374f6882a1e484a0117e2c
Lior <levyaz@post.bgu.ac.il>
2022-01-02 20:30:51 +0200

    push for encode

--- spl-net/Server/src/main/java/bgu/spl/net/api/EncDec.java
+++ spl-net/Server/src/main/java/bgu/spl/net/api/EncDec.java
@@ -29,95 +29,108 @@ public class EncDec<T> implements MessageEncoderDecoder<T>{
         int length=1;
         int counter;
 
-            if(opcode=="NOTIFICATION") {
-                length=opcode.length();
+        switch (opcode) {
+            case "NOTIFICATION": {
+                length = opcode.length();
 
-                char notificationType = messageS.substring(length+1, messageS.indexOf(" ")).charAt(0);
+                char notificationType = messageS.substring(length + 1, messageS.indexOf(" ")).charAt(0);
                 length++;//the notificationType is 0/1 - one step foreword
+
                 String postingUser = messageS.substring(messageS.indexOf(" ") + length);
                 byte[] postingUserBytes = postingUser.getBytes();
                 length = length + postingUser.length() + 1;
                 String Content = messageS.substring(messageS.indexOf(" ") + length);
                 byte[] ContentByts = Content.getBytes();
                 output = new byte[5 + postingUserBytes.length + ContentByts.length];
-                String [] number=new String[]{"0","9",Character.toString(notificationType)};
+                String[] number = new String[]{"0", "9", Character.toString(notificationType)};
                 this.counter = 3;
-                output=initializationOpcode(number,output);
-                String[] messageOutput={postingUser,Content};
-                output=bytes(messageOutput,output);
-                output[output.length-1] = 0;
+                initializationOpcode(number, output);
+                String[] messageOutput = {postingUser, Content};
+                bytes(messageOutput, output);
+                output[output.length - 1] = 0;
                 return output;
             }
+            case "ACK":
+                length = opcode.length();
+                String MessageOpcode = messageS.substring(length + 1, messageS.indexOf(" "));//TODO check if charat 1/0
+
+                String outputMessage = "";
+                if (MessageOpcode.equals("1") | MessageOpcode.equals("2") | MessageOpcode.equals("3") | MessageOpcode.equals("5") | MessageOpcode.equals("6")) {
+                    switch (MessageOpcode) {
+                        case "1":
+                            outputMessage = "successful register";
+                            break;
+                        case "2":
+                            outputMessage = "successful Login";
+                            break;
+                        case "3":
+                            outputMessage = "LOGOUT";
+                            break;
+                        case "5":
+                            outputMessage = "successful POST Messages";
+                            break;
+                        case "6":
+                            outputMessage = "successful PM Messages";
+                            break;
+                    }
+
+                    output = toByetsOutput(MessageOpcode, outputMessage);
+                    return output;
 
 
-            else if(opcode=="ACK")
-            {
-                length=opcode.length();
-                String MessageOpcode=messageS.substring(length+1,messageS.indexOf(" "));//TODO check if charat 1/0
-                String outputMessage="";
-                if(MessageOpcode=="1"|MessageOpcode=="2"|MessageOpcode=="3"|MessageOpcode=="5"|MessageOpcode=="6"){
-                if(MessageOpcode=="1")
-                    outputMessage="successful register";
-                else if(MessageOpcode=="2")
-                    outputMessage="successful Login";
-                else if(MessageOpcode=="3")
-                    outputMessage="LOGOUT";
-                else if(MessageOpcode=="5")
-                    outputMessage="successful POST Messages";
-                else if(MessageOpcode=="6")
-                    outputMessage="successful PM Messages";
-
-                output=toByetsOutput(MessageOpcode,outputMessage);
-                return output;}
-
-                else if(MessageOpcode=="4"){
-                    String[] messageOutput=new String[1];
-                    messageOutput[0]=messageS.substring(4, messageS.indexOf(" "));
+                } else if (MessageOpcode.equals("4")) {
+                    String[] messageOutput = new String[1];
+                    messageOutput[0] = messageS.substring(4, messageS.indexOf(" "));
                     byte[] mesBackByte = messageOutput[0].getBytes();
-                    output=new byte[4+mesBackByte.length];
-                    String [] number=new String[]{"1","0","0","4"};
-                    this.counter=4;
-                    output=initializationOpcode(number,output);
-                    output=bytes(messageOutput,output);
-                    output[output.length-1]=Byte.parseByte("0");
+                    output = new byte[4 + mesBackByte.length];
+                    String[] number = new String[]{"1", "0", "0", "4"};
+                    this.counter = 4;
+                    initializationOpcode(number, output);
+                    bytes(messageOutput, output);
+                    output[output.length - 1] = Byte.parseByte("0");
+                    return output;
+
+
+
+                } else if (MessageOpcode.equals("7") | MessageOpcode.equals("8"))
+                {
+                    String[] number = new String[4];
+                    if (MessageOpcode.equals("7")) {
+                        number = new String[]{"1", "0", "0", "7"};
+                    } else number = new String[]{"1", "0", "0", "8"};
+                    output = new byte[12];
+                    initializationOpcode(number, output);
+                    this.counter = 4;
+                    String[] Messages = new String[4];
+                    Messages[0] = messageS.substring(4, messageS.indexOf(" "));//age
+                    length = 4 + Messages[0].length() + 1;
+                    Messages[1] = messageS.substring(length, messageS.indexOf(" "));//numPost
+                    length += Messages[1].length() + 1;
+                    Messages[2] = messageS.substring(length, messageS.indexOf(" "));//NumFollowers
+                    length += Messages[2].length() + 1;
+                    Messages[3] = messageS.substring(length, messageS.indexOf(" "));//NumFollowing
+                    bytes(Messages, output);
                     return output;
                 }
-                else if(MessageOpcode=="7"|MessageOpcode=="8"){
-                    String [] number=new String[4];
-                    if(MessageOpcode=="7"){
-                        number= new String[]{"1", "0", "0", "7"};}
-                    else   number = new String[]{"1", "0", "0", "8"};
-                    output=new byte[12];
-                    output=initializationOpcode(number,output);
-                    this.counter=4;
-                    String[] Messages= new String[4];
-                    Messages[0]=messageS.substring(4, messageS.indexOf(" "));//age
-                    length=4+Messages[0].length()+1;
-                    Messages[1]=messageS.substring(length, messageS.indexOf(" "));//numPost
-                    length+=Messages[1].length()+1;
-                    Messages[2]=messageS.substring(length, messageS.indexOf(" "));//NumFollowers
-                    length+=Messages[2].length()+1;
-                    Messages[3]=messageS.substring(length, messageS.indexOf(" "));//NumFollowing
-                    output= bytes(Messages,output);
-                    return  output;
-                }
-            }
-            else if(opcode=="ERROR"){
-                length=opcode.length();
-                String mesBack=messageS.substring(messageS.indexOf(" ")+1);
+
+
+
+            case "ERROR": {
+                length = opcode.length();
+                String mesBack = messageS.substring(messageS.indexOf(" ") + 1);
                 byte[] mesBackByte = mesBack.getBytes();
-                output=new byte[4+mesBackByte.length];
-                String [] number=new String[]{"1","0","1","1"};
-                output=initializationOpcode(number,output);
-                counter=4;
-                for (int i =0;i<mesBackByte.length;i++,counter++)
-                {
-                    output[counter]=mesBackByte[i];
+                output = new byte[4 + mesBackByte.length];
+                String[] number = new String[]{"1", "0", "1", "1"};
+                initializationOpcode(number, output);
+                counter = 4;
+                for (int i = 0; i < mesBackByte.length; i++, counter++) {
+                    output[counter] = mesBackByte[i];
                 }
-                output[counter]=Byte.parseByte("0");
+                output[counter] = Byte.parseByte("0");
                 return output;
 
             }
+        }
             /*
             else if(opcode==12){
                 String[] messageOutput=new String[1];
@@ -154,14 +167,13 @@ public class EncDec<T> implements MessageEncoderDecoder<T>{
         return output;
     }
 
-    private byte[] initializationOpcode(String [] number,byte[] output){
+    private void initializationOpcode(String [] number, byte[] output){
         for (int i=0;i<number.length;i++){
             output[i]=Byte.parseByte(number[i]);
         }
-        return  output;
     }
 
-    private byte[] bytes(String[] messageOutput,byte[] output)
+    private void bytes(String[] messageOutput, byte[] output)
     {
         for (String s : messageOutput) {
             byte[] outputMessageByte = s.getBytes();
@@ -169,8 +181,7 @@ public class EncDec<T> implements MessageEncoderDecoder<T>{
                 output[counter] = outputMessageByte[i];
             }
         }
-            return output;
-        }
+    }
 
 
     private void pushByte(byte nextByte) {
